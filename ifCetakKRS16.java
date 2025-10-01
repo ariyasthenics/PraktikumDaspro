9ea4ce85fa261d429137498dd84ec94c604d8085
ariyasthenics <email@thekillercorona@gmail.com>
2025-10-01 18:52:40 +0700

    tugas 1

--- ifCetakKRS16.java
+++ ifCetakKRS16.java
@@ -4,7 +4,7 @@ public class ifCetakKRS16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.println("Aakah UKT sudah lunas? (true/false): ");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean UKTLunas = sc.nextBoolean();
         System.out.println(UKTLunas ? "ukt terverivikasi ":" ukt tidak terverivikasi");
         
