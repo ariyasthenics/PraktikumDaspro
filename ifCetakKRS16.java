278cf32174abe8b17db887abaeb4127bf2377619
ariyasthenics <email@thekillercorona@gmail.com>
2025-10-01 18:51:23 +0700

    tugas 1

--- ifCetakKRS16.java
+++ ifCetakKRS16.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-import javax.print.DocFlavor.STRING;
 public class ifCetakKRS16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
