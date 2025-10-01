f3771a97168f1b01f56138e376c754301497dd0a
ariyasthenics <email@thekillercorona@gmail.com>
2025-10-01 18:19:49 +0700

    Tugas 1

--- ifCetakKRS16.java
+++ ifCetakKRS16.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
 public class ifCetakKRS16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Aakah UKT sudah lunas? (true/false): ");
         boolean UKTLunas = sc.nextBoolean();
-        if (UKTLunas) {
-            System.out.println("Pembayaran UKT terverivikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        }
+        System.out.println(UKTLunas ? "ukt terverivikasi ":" ukt tidak terverivikasi");
+        
+
     }
 }
