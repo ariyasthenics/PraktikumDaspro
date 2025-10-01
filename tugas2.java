3cceecb3512deddf8bed698216ad110a7fb13164
ariyasthenics <email@thekillercorona@gmail.com>
2025-10-01 19:57:50 +0700

    berubahlah

---
+++ tugas2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class tugas2 {
+    public static void main(String[] args) {
+        int angka ;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("masukkan angka = ");
+        angka = sc.nextInt();
+
+        if (angka %2==) {
+            System.out.println("genap");
+        } else {
+            System.out.println("ganjil");
+        }
+    }
+}
